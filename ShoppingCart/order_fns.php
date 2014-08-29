75bd58a5506836506e94dd5b8ffaf493bc0b5fb1
Ethan <ethanmdt@gmail.com>
2014-08-29 23:54:59 +0800

    new order_fns

--- ShoppingCart/order_fns.php
+++ ShoppingCart/order_fns.php
@@ -36,6 +36,54 @@ function insert_order($order_details) {
 		}
 	}
 
-	$customerid = $conn->
+	$customerid = $conn->insert_id;
+
+	$date = date("Y-m-d");
+
+	$query = "insert into orders values 
+			  ('',$customerid', '".$_SESSION['total_price']."', '$date', 'PARTIAL', '$name2', '$address2', '$city2', 
+			  	'$state2', '$postal2', '$country2')";
+	
+	$result = $conn ->query($query);
+
+	if (!$result) {
+		return false;
+	}
+
+	$query = "select orderid from orders where
+			  customerid = '$customerid' and 
+			  amount > (".$_SESSION['total_price']."-.001) and
+			  amount < (".$_SESSION['total_price']."+.001) and
+			  date = '$date' and order_status ='PARTIAL' and ship_name = '$name2' and 
+			  ship_address = '$address2' and ship_city = '$city2' and ship_state = '$state2' and 
+			  ship_zip = '$postal2' and ship_country = '$country2'";
+	
+	$result = $conn -> query($query);
+
+	if (!$result -> num_rows) {
+		$order = $result -> fetch_object();
+		$orderid = $order -> orderid;
+	}  else {
+		return false;
+	}
+
+	foreach ($_SESSION['cart'] as $isbn => $quantity) {
+		$detail = get_book_details($isbn);
+		$query = "delete from order_items where
+				  orderid ='$orderid' and isbn = '$isbn'";
+		$result = $conn -> query($query);
+		$query = "insert into order_items values
+				  ('$orderid', '$isbn', '".$_detail['price'].",$quantity");
+		$result = $conn->query($query);
+		if (!$result) {
+			return false;
+		}
+	}
+
+	$conn -> commit();
+	$conn->autocommit(TRUE);
+
+	return $orderid;
+
 }
 ?>
